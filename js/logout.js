8e562f78716709af985f0bea93403d4b0140aed8
trilaanh2k3 <trinhminhtri0509@gmail.com>
2025-01-04 05:02:31 +0700

    Delete file js

--- js/logout.js
+++
@@ -1,8 +0,0 @@
-//this will make confirm box and if true it will make href of element to logut.php for redirection
-var element1 = document.getElementById("lg-btn");
-element1.addEventListener("click", () => {
-  var logout = confirm("Are you sure to logout?");
-  if (logout) {
-    element1.href = "logout.php"; //means if user click ok than only it will set logout href
-  }
-});
