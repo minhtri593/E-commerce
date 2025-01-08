48bcb57a5a01ce105b22b497130b6dc451fd25af
trilaanh2k3 <trinhminhtri0509@gmail.com>
2025-01-08 21:51:37 +0700

    Add contact and comment feature

---
+++ views/contact.php
@@ -0,0 +1,67 @@
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="./index.html"><i class="fa fa-home"></i> Home</a>
+                        <span>Contact</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Breadcrumb End -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__content">
+                        <div class="contact__address">
+                            <h5>THÔNG TIN LIÊN LẠC</h5>
+                            <ul>
+                                <li>
+                                    <h6><i class="fa fa-map-marker"></i> Địa chỉ</h6>
+                                    <p>Hutech Khu E Xa lộ Hà Nội</p>
+                                </li>
+                                <li>
+                                    <h6><i class="fa fa-phone"></i> Sô điện thoại</h6>
+                                    <p><span>0336216546</span><span>0909135569</span></p>
+                                </li>
+                                <li>
+                                    <h6><i class="fa fa-headphones"></i> Hỗ trợ</h6>
+                                    <p>Support.hutech@gmail.com</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="contact__form">
+                            <h5>
+                                GỬI TIN NHẮN</h5>
+                            <form action="#">
+                                <input type="text" placeholder="Họ tên">
+                                <input type="text" placeholder="Email">
+                                <input type="text" placeholder="Website">
+                                <textarea placeholder="Tin nhắn"></textarea>
+                                <button type="submit" class="site-btn">GỬI TIN NHẮN</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__map">
+                        <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.1447789860434!2d106.7847038!3d10.8550348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527006db97ff1%3A0x8ed7036831a229d3!2sHUTECH%20E%202!5e0!3m2!1sen!2s!4v1698323706006!5m2!1sen!2s"
+                        height="780" style="border:0" allowfullscreen="">
+                        </iframe>
+                        
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->    
+
